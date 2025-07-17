716ade817f048e9b2f12da95419b3e7c59901b7e
Naman Singh <singhnaman11298@gmail.com>
2025-07-17 21:48:48 +0530

    array

---
+++ src/Array/RotateArrayByRandomPlace.kt
@@ -0,0 +1,16 @@
+package Array
+
+fun main(){
+
+    print("Left Rotate array by random place\n")
+    val arr = arrayOf(1,2,3,4,5,6,7,8,9,0)
+    leftRotateByRandom(arr, 2)
+}
+
+fun leftRotateByRandom(arr: Array<Int>, n: Int) {
+
+    var rotateArray = Array(arr.size){0}
+
+
+    for()
+}
