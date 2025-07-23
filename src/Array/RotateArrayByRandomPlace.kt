58f8c5d82dbd332463d4ef2e2cf9c5bc5f263b0f
Naman Singh <singhnaman11298@gmail.com>
2025-07-23 06:16:07 +0530

    Coding

--- src/Array/RotateArrayByRandomPlace.kt
+++ src/Array/RotateArrayByRandomPlace.kt
@@ -3,14 +3,56 @@ package Array
 fun main(){
 
     print("Left Rotate array by random place\n")
-    val arr = arrayOf(1,2,3,4,5,6,7,8,9,0)
-    leftRotateByRandom(arr, 2)
+    val arr = arrayOf(1,2,3,4,5,6,7,8,9,10)
+    leftRotateByRandomNative(arr, 4)
+}
+
+fun leftRotateByRandomNative(arr: Array<Int>, n: Int) {
+
+    val size = arr.size
+    val rotateBy = n % size  // to handle n > size
+    val rotateArray = Array(rotateBy) { 0 }
+
+    // Step 1: Backup first 'rotateBy' elements
+    for (i in 0 until rotateBy) {
+        rotateArray[i] = arr[i]
+    }
+
+    // Step 2: Shift elements left by 'rotateBy' places
+    for (i in 0 until size - rotateBy) {
+        arr[i] = arr[i + rotateBy]
+    }
+
+    // Step 3: Add backup elements to the end
+    for (i in 0 until rotateBy) {
+        arr[size - rotateBy + i] = rotateArray[i]
+    }
+
+    for(a in arr){
+        print(" $a")
+    }
+
 }
 
 fun leftRotateByRandom(arr: Array<Int>, n: Int) {
+    val size = arr.size
+    val rotateBy = n % size  // in case n > size
 
-    var rotateArray = Array(arr.size){0}
+    // Backup first 'rotateBy' elements
+    val temp = arr.sliceArray(0 until rotateBy)
 
+    // Shift remaining elements to the left
+    for (i in 0 until size - rotateBy) {
+        arr[i] = arr[i + rotateBy]
+    }
 
-    for()
+    // Place the backed-up elements at the end
+    for (i in 0 until rotateBy) {
+        arr[size - rotateBy + i] = temp[i]
+    }
+
+    // Print result
+    for (value in arr) {
+        print(" $value")
+    }
 }
