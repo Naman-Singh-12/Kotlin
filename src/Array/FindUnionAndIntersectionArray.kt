e4e9432b09c7ab9961b420ade1c2889011330b25
Naman Singh <singhnaman11298@gmail.com>
2025-08-01 18:15:22 +0530

    Union and Intersection

--- src/Array/FindUnionAndIntersectionArray.kt
+++ src/Array/FindUnionAndIntersectionArray.kt
@@ -1,2 +1,22 @@
 package Array
 
+fun main(){
+    print("Find Union and Intersection of two Arrays")
+    val arr1 = arrayOf(1,2,3,4,5)
+    val arr2 = arrayOf(4,5,6,7,8,9)
+    findUnionAndIntersection(arr1, arr2)
+}
+
+fun findUnionAndIntersection(arr1: Array<Int>, arr2: Array<Int>) {
+
+    var unionArr = ""
+    var intersectionArr = ""
+
+    for(i in 0 until arr1.size){
+
+        for(j in 0 until arr2.size){
+
+        }
+    }
+
+}
