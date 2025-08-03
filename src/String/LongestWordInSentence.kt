71de42e963e0a51e47f2bab5dafc7e4c15546ccb
Naman Singh <singhnaman11298@gmail.com>
2025-08-03 17:20:03 +0530

    Union and Intersection

---
+++ src/String/LongestWordInSentence.kt
@@ -0,0 +1,29 @@
+package String
+
+fun main(){
+    println("Find Longest word in sentence")
+    val str = "hey how are you. I hope every things goes well"
+    findLongestWord(str)
+}
+
+fun findLongestWord(str: String){
+    var currentWordLength = 0
+    var lastLargestWord = 0
+    var word = ""
+    for(i in 0 until str.length){
+
+        if(str[i].isLetter()){
+
+            currentWordLength++
+        }else{
+            if(lastLargestWord < currentWordLength) {
+                lastLargestWord = currentWordLength
+                word = str.substring(i-currentWordLength, i )
+            }
+            currentWordLength = 0
+        }
+
+    }
+
+    println("$word : $lastLargestWord")
+}
