0a735308dbae3cdf2c6a9aab4ee494dbe2da8977
Naman Singh <singhnaman11298@gmail.com>
2025-09-29 10:35:50 +0530

    Remove duplicate in Collection

---
+++ src/Collection/RemoveDuplicate.kt
@@ -0,0 +1,23 @@
+package Collection
+
+
+fun main() {
+
+    print("Remove Duplicate in Collection\n")
+    val number = listOf(1, 4, 2, 3, 1, 5, 8, 3, 7, 2, 4)
+    val name = listOf("Naman", "Raj", "Raj", "Anmol", "Naman")
+
+
+    // By using list function
+    val removeDuplicate = number.distinct()
+
+    println("Original List: $number")
+    println("Unique Number List: $removeDuplicate")
+    println("Original List: $name")
+    println("Unique Name List: ${name.distinct()}")
+
+    // by converting Set
+    println("Unique Number List: ${number.toSet().toList()}")
+    println("Unique Name List: ${name.toSet().toList()}")
+
+}
